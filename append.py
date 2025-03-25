56a2b21cfaa838ec4e2fa5644939abd7ddcad937
Yan <yantmartins@live.com>
2025-03-24 21:50:12 -0400

    up

--- append.py
+++ append.py
@@ -4,8 +4,9 @@ a.sort() #Ordena os numeros da lista
 a.sort(reverse=True) #Coloca a ordem reversa na lista
 print(a)
 
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 88, 18]
 print(b.index(4)) ##Busca a posição do elemento do indice passado
 
 b.insert(1,100) #Insere o segundo parametro na posicao do primeiro parametro
 print(b)
+print(b.count(8)) ##Conta quantas vezes o parametro passado aparece na lista
