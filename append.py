d0d44fd154a50f905d2dabd7c8ae7957df17ca6f
Yan <yantmartins@live.com>
2025-03-24 21:47:05 -0400

    news

---
+++ append.py
@@ -0,0 +1,11 @@
+a = [81, 82, 83]
+a.append(5) #Adiciona o parametro no final da lista
+a.sort() #Ordena os numeros da lista 
+a.sort(reverse=True) #Coloca a ordem reversa na lista
+print(a)
+
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(b.index(4)) ##Busca a posição do elemento do indice passado
+
+b.insert(1,100) #Insere o segundo parametro na posicao do primeiro parametro
+print(b)
