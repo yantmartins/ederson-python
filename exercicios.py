266e91cff1409d671c0079e1649d7ca9f7fa467d
Yan <yantmartins@live.com>
2025-03-26 21:10:45 -0400

    up codigos

---
+++ exercicios.py
@@ -0,0 +1,24 @@
+#Exercicio 1
+lista = [1,2,3,4,5]
+print(lista)
+print(max(lista))
+print(min(lista))
+print(len(lista))
+print(sum(lista))
+
+#Exercicio 2
+lista2 = [1,2,1,5,6,4,3,1]
+print(lista2.count(1))
+
+#Exercicio 3
+frutas = ['Banana','Uva','Pera','Kiwi','Morango']
+frutas[1] = 'Laranja' #Substitui o segundo elemento na lista
+print(frutas)
+
+#Exercicio 4
+t = [[1,2],[3],[4,5,6]]
+
+print(sum(sum(t, [])))
+               
+#Exercicio 5
+               
---
+++ extend.py
@@ -0,0 +1,3 @@
+lista = [1,2,3]
+lista.extend([4,5]) #Adiciona os parametros ao fim da lista
+print(lista)
