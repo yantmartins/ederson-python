b7f0f0be03623e2c0e347c9df2bc3d3ac4441e44
Yan <yantmartins@live.com>
2025-03-31 21:20:36 -0400

    mudancas

---
+++ thiago/arquivo.py
@@ -0,0 +1,9 @@
+a = input("Digite seu nome: ")
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+
+resultado = num1 + num2
+
+print(resultado)
+print(a)
---
+++ thiago/arquivo2.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print ("Pode tirar a carteira!")
+else:
+    print("Nao pode tirar carteira!")    
