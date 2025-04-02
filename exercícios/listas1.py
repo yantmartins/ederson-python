3c1c2dcc7924eb44573a305df08ab08e155550e0
Yan <yantmartins@live.com>
2025-04-02 19:56:08 -0400

    novos exercícios

---
+++ exercícios/listas1.py
@@ -0,0 +1,6 @@
+listas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(listas[1:10])
+print(listas[8:])
+print(listas[::2])
+print(listas[::-1])
---
+++ exercícios/listas2.py
@@ -0,0 +1,11 @@
+
+P1 = [8.7, 5.3, 10.0, 6.5, 9.3]
+P2 = [4.5, 7.9, 9.0, 7.5, 8.8]
+
+
+media_p1 = sum(P1) / len(P1)
+media_p2 = sum(P2) / len(P2)
+
+
+print(f"Média da turma na prova 1: {media_p1:.2f}")
+print(f"Média da turma na prova 2: {media_p2:.2f}")
