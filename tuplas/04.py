e0702af6ec6e6ee934e6ed815e5a2f3c72deeba7
Yan <yantmartins@live.com>
2025-04-07 21:53:03 -0400

    exercicio

---
+++ tuplas/04.py
@@ -0,0 +1,19 @@
+print("-- Reservatório de Água --") 
+
+altura = float(input("Digite a altura (cm): ")) 
+largura = float(input("Digite a largura (cm): ")) #Faltava um parenteses e estava como INT
+comprimento = float(input("Digite o comprimento (cm): ")) #Faltava sinal de = 
+c_diario = float(input("Digite o valor do consumo médio diário (litros/dia): "))
+
+cap_total = (altura * largura * comprimento) / 1000 # Comentario com as aspas triplas
+auton_reser = cap_total / c_diario
+
+print(f"\nCapacidade do Reservatório: {cap_total:.2f} litros")
+print(f"Autonomia do Reservatório: {auton_reser:.2f} dias")
+
+if auton_reser < 2:
+    print("Consumo Elevado")
+elif 2 <= auton_reser <= 7:
+    print("Consumo Moderado")
+else:
+    print("Consumo Baixo")
