d29c8d64bf2c221ebb037d5aaec5a09c3d194e74
Yan <yantmartins@live.com>
2025-04-07 21:38:14 -0400

    tuplas

---
+++ tuplas/01.py
@@ -0,0 +1,8 @@
+##Tuplas são listas que não podem ser modificadas e usamos () ao invés de [].
+
+got = ("Game of Thrones", 2011, 2019, 9.4)
+print(got)
+
+print(type(got))
+
+print(len(got))
---
+++ tuplas/03.py
@@ -0,0 +1,7 @@
+idade = int(input ("Digite sua idade: "))
+if idade >=16 and idade <18:
+    print("Você está apto a votar")
+elif idade < 16:
+    print("Apenas estude")
+else:
+    print("Pode dirigir")        
