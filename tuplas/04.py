191fe5f70947f4dea1f72f6281c6a7ef45f00965
Yan <yantmartins@live.com>
2025-04-08 21:42:53 -0400

    upuupup

--- tuplas/04.py
+++ tuplas/04.py
@@ -8,11 +8,11 @@ c_diario = float(input("Digite o valor do consumo médio diário (litros/dia): "
 cap_total = (altura * largura * comprimento) / 1000 # Comentario com as aspas triplas
 auton_reser = cap_total / c_diario
 
-print(f"\nCapacidade do Reservatório: {cap_total:.2f} litros")
+print(f"\nCapacidade do Reservatório: {cap_total:.2f} litros") #melhor formatacao de saida
 print(f"Autonomia do Reservatório: {auton_reser:.2f} dias")
 
 if auton_reser < 2:
-    print("Consumo Elevado")
+    print("Consumo Elevado") #identacao corrigida 
 elif 2 <= auton_reser <= 7:
     print("Consumo Moderado")
 else:
