7db283b286c34ff5ec766abde937906765d1f38e
Yan <yantmartins@live.com>
2025-05-07 21:34:04 -0400

    up for

---
+++ explicacoes/for.py
@@ -0,0 +1,29 @@
+#Exemplo 1
+frutas = ["maca", "banana","laranja"]
+for i in frutas:
+    print(i)
+
+print("fim")
+
+#Exemplo 2
+nomes = ["Pedro","Joao","Leticia"]
+
+for n in nomes:
+    print(n)
+
+#Exemplo 3
+nomes = ["Pedro","Joao","Leticia"]
+
+for n in nomes:
+    print(n)
+    if n == "Joao":    
+        break
+
+#Exemplo 4
+for x in range(2,10,2):
+    print(x)
+
+#Exemplo 5
+for i in range(5):
+    for j in range(6):
+        print(i,j)
